d8247a615008dd9b584140e33b9a5b1f41546eaa
Irfanabdul1108 <143101267+Irfanabdul1108@users.noreply.github.com>
2024-06-20 22:13:37 +0530

    Add files via upload

---
+++ maxandmin.py
@@ -0,0 +1,6 @@
+    # platform - geeksforgeeks
+#link - https://www.geeksforgeeks.org/problems/max-min/1?itm_source=geeksforgeeks&itm_medium=article&itm_campaign=bottom_sticky_on_article
+# concept - max and min
+
+    def findSum(self,A,N): 
+           return min(A)+max(A);
